b947798034f6992802df93f8090f778a082a0b77
mahdez <mahdee.dark@gmail.com>
2018-03-23 03:18:19 +0700

    ei ei

--- image-processing/Form1.Designer.cs
+++ image-processing/Form1.Designer.cs
@@ -29,44 +29,153 @@
         private void InitializeComponent()
         {
             this.button1 = new System.Windows.Forms.Button();
+            this.btn_mean_filter = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.txt_Q = new System.Windows.Forms.TextBox();
+            this.btn_max = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.btn_min = new System.Windows.Forms.Button();
+            this.btn_midpoint = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
+            this.txt_d = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(38, 276);
+            this.button1.Location = new System.Drawing.Point(12, 321);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(84, 30);
             this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
+            this.button1.Text = "Open File";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btn_mean_filter
+            // 
+            this.btn_mean_filter.Location = new System.Drawing.Point(187, 314);
+            this.btn_mean_filter.Name = "btn_mean_filter";
+            this.btn_mean_filter.Size = new System.Drawing.Size(84, 51);
+            this.btn_mean_filter.TabIndex = 1;
+            this.btn_mean_filter.Text = "Contra Harmonic Mean Filter";
+            this.btn_mean_filter.UseVisualStyleBackColor = true;
+            this.btn_mean_filter.Click += new System.EventHandler(this.btn_mean_filter_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 26);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(238, 162);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(314, 26);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(238, 162);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox2.TabIndex = 3;
+            this.pictureBox2.TabStop = false;
+            // 
+            // txt_Q
+            // 
+            this.txt_Q.Location = new System.Drawing.Point(113, 327);
+            this.txt_Q.Name = "txt_Q";
+            this.txt_Q.Size = new System.Drawing.Size(68, 20);
+            this.txt_Q.TabIndex = 4;
+            // 
+            // btn_max
+            // 
+            this.btn_max.Location = new System.Drawing.Point(277, 327);
+            this.btn_max.Name = "btn_max";
+            this.btn_max.Size = new System.Drawing.Size(75, 23);
+            this.btn_max.TabIndex = 5;
+            this.btn_max.Text = "Max";
+            this.btn_max.UseVisualStyleBackColor = true;
+            this.btn_max.Click += new System.EventHandler(this.btn_max_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // btn_min
+            // 
+            this.btn_min.Location = new System.Drawing.Point(358, 327);
+            this.btn_min.Name = "btn_min";
+            this.btn_min.Size = new System.Drawing.Size(75, 24);
+            this.btn_min.TabIndex = 6;
+            this.btn_min.Text = "Min";
+            this.btn_min.UseVisualStyleBackColor = true;
+            this.btn_min.Click += new System.EventHandler(this.btn_min_Click);
+            // 
+            // btn_midpoint
+            // 
+            this.btn_midpoint.Location = new System.Drawing.Point(439, 328);
+            this.btn_midpoint.Name = "btn_midpoint";
+            this.btn_midpoint.Size = new System.Drawing.Size(75, 23);
+            this.btn_midpoint.TabIndex = 7;
+            this.btn_midpoint.Text = "Midpoint";
+            this.btn_midpoint.UseVisualStyleBackColor = true;
+            this.btn_midpoint.Click += new System.EventHandler(this.btn_midpoint_Click);
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(139, 276);
+            this.button2.Location = new System.Drawing.Point(521, 328);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(84, 30);
-            this.button2.TabIndex = 1;
-            this.button2.Text = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "Alpha";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // txt_d
+            // 
+            this.txt_d.Location = new System.Drawing.Point(521, 293);
+            this.txt_d.Name = "txt_d";
+            this.txt_d.Size = new System.Drawing.Size(75, 20);
+            this.txt_d.TabIndex = 9;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(588, 335);
+            this.ClientSize = new System.Drawing.Size(652, 375);
+            this.Controls.Add(this.txt_d);
             this.Controls.Add(this.button2);
+            this.Controls.Add(this.btn_midpoint);
+            this.Controls.Add(this.btn_min);
+            this.Controls.Add(this.btn_max);
+            this.Controls.Add(this.txt_Q);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btn_mean_filter);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btn_mean_filter;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.TextBox txt_Q;
+        private System.Windows.Forms.Button btn_max;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button btn_min;
+        private System.Windows.Forms.Button btn_midpoint;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox txt_d;
     }
 }
 
--- image-processing/Form1.cs
+++ image-processing/Form1.cs
@@ -12,9 +12,246 @@ namespace image_processing
 {
     public partial class Form1 : Form
     {
+        int[,] imageInput;
+        int d = 0;
+        private Bitmap f_image = null;
+        public Bitmap image;
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.Filter = "bitmap (*.bmp)|*.bmp";
+            openFileDialog1.FilterIndex = 1;
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    if (f_image != null) f_image.Dispose();
+                    f_image = (Bitmap)Bitmap.FromFile(openFileDialog1.FileName, false);
+
+                }
+                catch (Exception)
+                {
+                    MessageBox.Show("Can not open file", "File Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            
+            }
+            image = new Bitmap(f_image.Width, f_image.Height);
+            imageInput = new int[f_image.Width, f_image.Height];
+            for (int i = 0; i < f_image.Height; i++)
+            {
+                for (int j = 0; j < f_image.Width; j++)
+                {
+                    Color PixelColor = f_image.GetPixel(j, i);
+                    int C_gray = (int)(PixelColor.R + PixelColor.G + PixelColor.B) / 3;
+                    imageInput[j, i] = C_gray;
+                    //  num_pic.Add(C_gray);
+                    image.SetPixel(j, i, Color.FromArgb(C_gray, C_gray, C_gray));
+
+                }
+            }
+            pictureBox1.Image = f_image;
+            pictureBox2.Image = image;
+        }
+
+        public int[,] colorInArray()
+        {
+            int[,] colorPic = new int[f_image.Width, f_image.Height];
+            for (int i = 0; i < f_image.Width; i++)
+            {
+                for (int j = 0; j < f_image.Height; j++)
+                {
+                    Color PixelColor = f_image.GetPixel(i, j);
+                    int C_gray = (int)(PixelColor.R + PixelColor.G +
+                    PixelColor.B) / 3;
+                    colorPic[i, j] = C_gray;
+                }
+            }
+            return colorPic;
+        }
+
+        private void btn_mean_filter_Click(object sender, EventArgs e)
+        {
+            int q = Convert.ToInt32(txt_Q.Text);
+
+            image = new Bitmap(f_image.Width, f_image.Height);
+            int[,] colorPic = new int[f_image.Width, f_image.Height];
+            colorPic = colorInArray();
+
+            for (int i = 0; i < f_image.Width; i++)
+            {
+                for (int j = 0; j < f_image.Height; j++)
+                {
+                    if (i == 0 || j == 0 || i == f_image.Width - 1 || j == f_image.Height - 1)
+                    {
+                        image.SetPixel(i, j, Color.FromArgb(colorPic[i, j], colorPic[i, j], colorPic[i, j]));
+                    }
+                    else
+                    {
+                        double sumtop = 0, sumbot = 0;
+                        int newColor = 0;
+                        for (int x = i - 1; x <= i; x++)
+                        {
+                            for (int y = j - 1; y <= j; y++)
+                            {
+                                Color PixelColor = f_image.GetPixel(x, y);
+                                int C_gray = (int)(PixelColor.R + PixelColor.G + PixelColor.B) / 3;
+                                if (C_gray != 0)
+                                {
+                                    sumbot += (double)Math.Pow(C_gray, q);
+                                    sumtop += (double)Math.Pow(C_gray, q + 1);
+                                }
+                            }
+                        }
+
+                        try
+                        {
+                            newColor = Convert.ToInt32(Math.Round(sumtop / sumbot));
+
+                        }
+                        catch (Exception ex)
+                        {
+                            newColor = 0;
+                        }
+
+                        image.SetPixel(i, j, Color.FromArgb(newColor, newColor, newColor));
+                    }
+
+                }
+            }
+            pictureBox2.Image = image;
+
+        }
+
+        private void btn_max_Click(object sender, EventArgs e)
+        {
+            for (int i = 1; i < f_image.Height - 1; i++)
+            {
+                for (int j = 1; j < f_image.Width - 1; j++)
+                {
+                    Color PixelColor = f_image.GetPixel(j, i);
+                    int C_gray = (int)(PixelColor.R + PixelColor.G + PixelColor.B) / 3;
+                    imageInput[j, i] = C_gray;
+                    //  num_pic.Add(C_gray);
+                    int max = Int32.MinValue;
+                    for (int m = i - 1; m < i + 2; m++)
+                    {
+                        for (int n = j - 1; n < j + 2; n++)
+                        {
+                            if (imageInput[n, m] > max)
+                            {
+                                max = imageInput[n, m];
+                            }
+                        }
+                    }
+                    image.SetPixel(j, i, Color.FromArgb(max, max, max));
+                }
+            }
+
+            pictureBox2.Image = image;
+        }
+
+        private void btn_min_Click(object sender, EventArgs e)
+        {
+            for (int i = 1; i < f_image.Height - 1; i++)
+            {
+                for (int j = 1; j < f_image.Width - 1; j++)
+                {
+                    Color PixelColor = f_image.GetPixel(j, i);
+                    int C_gray = (int)(PixelColor.R + PixelColor.G + PixelColor.B) / 3;
+                    imageInput[j, i] = C_gray;
+                    //  num_pic.Add(C_gray);
+                    int min = Int32.MaxValue;
+                    for (int m = i - 1; m < i + 2; m++)
+                    {
+                        for (int n = j - 1; n < j + 2; n++)
+                        {
+                            if (imageInput[n, m] < min)
+                            {
+                                min = imageInput[n, m];
+                            }
+                        }
+                    }
+                    image.SetPixel(j, i, Color.FromArgb(min, min, min));
+                }
+            }
+
+            pictureBox2.Image = image;
+        }
+
+        private void btn_midpoint_Click(object sender, EventArgs e)
+        {
+            for (int i = 1; i < f_image.Height - 1; i++)
+            {
+                for (int j = 1; j < f_image.Width - 1; j++)
+                {
+                    Color PixelColor = f_image.GetPixel(j, i);
+                    int C_gray = (int)(PixelColor.R + PixelColor.G + PixelColor.B) / 3;
+                    imageInput[j, i] = C_gray;
+                    //  num_pic.Add(C_gray);
+                    int max = Int32.MinValue;
+                    int min = Int32.MaxValue;
+                    int mid = 0;
+                    for (int m = i - 1; m < i + 2; m++)
+                    {
+                        for (int n = j - 1; n < j + 2; n++)
+                        {
+                            if (imageInput[n, m] > max)
+                            {
+                                max = imageInput[n, m];
+                            }
+                            if (imageInput[n, m] < min)
+                            {
+                                min = imageInput[n, m];
+                            }
+                        }
+                        mid = (max + min) / 2;
+                    }
+                    image.SetPixel(j, i, Color.FromArgb(mid, mid, mid));
+                }
+            }
+
+            pictureBox2.Image = image;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int d = Convert.ToInt16(txt_d.Text);
+            image = new Bitmap(f_image.Width, f_image.Height);
+            int[,] colorPic = new int[f_image.Width, f_image.Height];
+            colorPic = colorInArray();
+
+            for (int i = 0; i < f_image.Width; i++)
+            {
+                for (int j = 0; j < f_image.Height; j++)
+                {
+                    if (i == 0 || j == 0 || i == f_image.Width - 1 || j == f_image.Height - 1)
+                    {
+                        image.SetPixel(i, j, Color.FromArgb(colorPic[i, j], colorPic[i, j], colorPic[i, j]));
+                    }
+                    else
+                    {
+                        int[] values = { colorPic[i-1,j-1],colorPic[i-1, j],colorPic[i-1, j+1],
+                            colorPic[i, j-1],colorPic[i, j],colorPic[i, j+1],
+                            colorPic[i+1, j-1], colorPic[i+1, j], colorPic[i+1, j+1] };
+
+                        Array.Sort(values);
+                        int sum = 0;
+                        int pop = d / 2;
+                        for (int k = pop; k < values.Length - pop; k++)
+                        {
+                            sum += values[k];
+                        }
+                        sum = sum / (9 - d);
+                        image.SetPixel(i, j, Color.FromArgb(sum, sum, sum));
+                    }
+
+                }
+            }
+            pictureBox2.Image = image;
         }
     }
 }
